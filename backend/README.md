2ce4c8110d397eaafee80a623f2f260ea08b5f76
Arthur Hovhannisyan <hovhannisyan.arthur@gmail.com>
2026-05-28 20:30:47 +0400

    chore: scaffold backend Spring Boot project

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+# Lva Backend
+
+Java 21 + Spring Boot 3 modular monolith.
+
+## Run locally
+docker compose up
+
+## Build
+./mvnw clean package -DskipTests
+
+## API docs
+http://localhost:8080/swagger-ui.html
---
+++ backend/src/main/java/am/lva/LvaApplication.java
@@ -0,0 +1,11 @@
+package am.lva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LvaApplication.class, args);
+    }
+}
